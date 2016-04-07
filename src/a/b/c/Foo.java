e4e7c572e471f27387372ae549816467e2f1ff18
irina[irs]petrovskaya <irs@foo.com>
2016-04-07 13:54:38 +0300

    add data class Foo

---
+++ src/a/b/c/Foo.java
@@ -0,0 +1,10 @@
+package a.b.c;
+
+/**
+ * *******************************
+ * Created by Irina.Petrovskaya on 4/7/2016.
+ * Project: modelAttrsResolving_xml
+ * *******************************
+ */
+public class Foo {
+}
